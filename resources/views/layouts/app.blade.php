efb1e0bcdab39bed4386e7b42922a61060c4d2f6
Faysal Ahamed <miufaysal@live.com>
2018-09-29 15:37:43 +0600

    logo updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
         <div class="container">
-            <a class="navbar-brand" href="{{route('home')}}"> <img src="{{asset('images/naukri_Logo.png')}}" class="img-fluid"></a>
+            <a class="navbar-brand" href="{{route('home')}}"> <img src="{{asset('images/onlinejobs-logo.png')}}" class="img-fluid"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
