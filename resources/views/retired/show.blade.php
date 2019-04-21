1059024460f4a5c563db3f7ed5b0dbf7613353e5
Faysal Ahamed <miufaysal@live.com>
2019-04-21 12:04:55 +0600

    Retired profile

--- resources/views/retired/show.blade.php
+++ resources/views/retired/show.blade.php
@@ -48,102 +48,99 @@
             </div>
         </div>
         <div class="col-md-12 mt-4">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4>Quick Links</h4>
-                            <hr>
-                            <p>Resume Headline</p>
-                            <p>Key Skills</p>
-                            <p>Employment</p>
-                            <p>Educaition</p>
-                            <p>IT SKills</p>
-                            <p>Personal Details</p>
-                            <p>Attach Resume</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-9">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4>Employment 
-                                @if(Auth::id() == $user->id)
-                                    <a class="text-black" href="{{route('retiredPersonnelExperience.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                                @endif
-                            </h4>
-                            @foreach($user->retired_personnel_experiences as $experience)
-                                <div class="mt-1">
-                                    <p class="mb-0">{{$experience->position}}</p>
-                                    <p class="mb-0">{{$experience->company_name}}</p>
-                                    <p class="mb-0">{{\Carbon\Carbon::parse($experience->from)->format('M-Y') }} to {{\Carbon\Carbon::parse($experience->to)->format('M-Y') }}</p>
+            <div class="card">
+                <div class="card-body">
+                    <div>
+                        <h3><i class="fa fa-briefcase" aria-hidden="true"></i> Experience
+                            @if(Auth::id() == $user->id)
+                                <a class="text-black" href="{{route('retiredPersonnelExperience.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                            @endif
+                        </h3>
+                        <p>Years of total experience</p>
+    
+                        @foreach($user->retired_personnel_experiences as $experience)
+                            <div class="row mb-3">
+                                <div class="col-md-3">
+                                    <p class="mb-0">{{\Carbon\Carbon::parse($experience->from)->format('M Y') }} - {{\Carbon\Carbon::parse($experience->to)->format('M Y') }}</p>
                                 </div>
-                                <hr>
-                            @endforeach
-                        </div>
-                    </div>
-                    <div class="card mt-4">
-                        <div class="card-body">
-                            <h4>Education
-                                @if(Auth::id() == $user->id)
-                                    <a class="text-black" href="{{route('retiredPersonnel.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                                @endif
-                            </h4>
-                            <p class="mb-0"><strong>Highest Academic Qualification:</strong> {{$user->retired_personnel->highest_academic_qualification}}</p>
-                            <p class="mb-0"><strong>Specialization:</strong> {{$user->retired_personnel->specialization}}</p>
-                            <hr>
-                            @foreach($user->retired_personnel_educations as $education)
-                                <div class="mt-1">
-                                    <p class="mb-0"><strong>Academic Qualification:</strong> {{$education->academic_qualification}}</p>
-                                    <p class="mb-0"><strong>Specialization:</strong> {{$education->specialization}}</p>
+                                <div class="col-md-9">
+                                    <p class="mb-0 font-20 font-weight-bold">{{$experience->position}}</p>
+                                    <p class="mb-0 font-20">{{$experience->company_name}} | {{$experience->address}}</p>
+                                    <p class="mb-0">Industry: {{$experience->nature_of_company_business}}</p>
+                                    <p class="mb-0">{{$experience->work_description}}</p>
                                 </div>
-                                <hr>
-                            @endforeach
-                        </div>
+                            </div>
+                        @endforeach
                     </div>
-                    @if($user->retired_personnel_language->count() > 0)
-                    <div class="card mt-4">
-                        <div class="card-body">
-                            <h4>Language 
-                                @if(Auth::id() == $user->id)
-                                    <a class="text-black" href="{{route('retiredPersonnelsLanguage.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                                @endif
-                            </h4>
-                            @foreach($user->retired_personnel_language as $language)
-                                <div class="mt-1">
-                                    <p class="mb-0">{{$language->language}}</p>
-                                    <p class="mb-0">{{$language->speaking}}</p>
-                                    <p class="mb-0">{{$language->writing}}</p>
-                                </div>
-                                <hr>
-                            @endforeach
-                        </div>
+                    <div class="mt-5">
+                        <h3><i class="fa fa-graduation-cap" aria-hidden="true"></i> Education
+                            @if(Auth::id() == $user->id)
+                            <a class="text-black" href="{{route('retiredPersonnel.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                            @endif
+                        </h3>
+    
+                        <p class="mb-0"><strong>Highest Academic Qualification:</strong> {{$user->retired_personnel->highest_academic_qualification}}</p>
+                        <p class="mb-0"><strong>Specialization:</strong> {{$user->retired_personnel->specialization}}</p>
+    
+                        @foreach($user->retired_personnel_educations as $education)
+                            <div class="mt-2">
+                                <p class="mb-0"><strong>Academic Qualification:</strong> {{$education->academic_qualification}}</p>
+                                <p class="mb-0"><strong>Specialization:</strong> {{$education->specialization}}</p>
+                            </div>
+                        @endforeach
                     </div>
-                    @endif
-                    <div class="card mt-4">
-                        <div class="card-body">
-                            <h4>Health 
-                                @if(Auth::id() == $user->id)
-                                    <a class="text-black" href="{{route('retiredPersonnel.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
+
+                    <div class="mt-5">
+                        <h3><i class="fa fa-language" aria-hidden="true"></i> Language
+                            @if(Auth::id() == $user->id)
+                            <a class="text-black" href="{{route('retiredPersonnelsLanguage.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                            @endif
+                        </h3>
+                        <p>Proficiency Level: Fluent > Good > Poor</p>
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>Language</th>
+                                    <th>Spoken</th>
+                                    <th>Written</th>
+                                </tr>
+                            </thead>
+                        
+                            <tbody>
+                                @if($user->retired_personnel_language->count() > 0)
+                                    @foreach($user->retired_personnel_language as $language)
+                                    <tr>
+                                        <td>{{$language->language}}</td>
+                                        <td>{{$language->speaking}}</td>
+                                        <td>{{$language->writing}}</td>
+                                    </tr>
+                                    @endforeach
                                 @endif
-                            </h4>
-                            <p class="mb-0"><strong>Fit to work ? </strong> {{$user->retired_personnel->fit_to_work }}</p>
-                            <p class="mb-0"><strong>Have blood pressure ? </strong> {{$user->retired_personnel->have_blood_pressure }}</p>
-                            <p class="mb-0"><strong>Have diabetes ? </strong> {{$user->retired_personnel->have_diabetes }}</p>
-                            <p class="mb-0"><strong>Additional Health Information: </strong> {{$user->retired_personnel->additional_health_statement }}</p>
-                        </div>
+                            </tbody>
+                        </table>
                     </div>
-                    <div class="card mt-4">
-                        <div class="card-body">
-                            <h4>Other Info 
-                                @if(Auth::id() == $user->id)
-                                    <a class="text-black" href="{{route('retiredPersonnel.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
-                                @endif
-                            </h4>
-                            <p class="mb-0"><strong>Were you government servent ? </strong> {{$user->retired_personnel->government_employee }}</p>
-                            <p class="mb-0"><strong>Prefer Working hours: </strong> {{$user->retired_personnel->full_time ? 'Full Time' : $user->retired_personnel->describe_working_hours}}</p>
-                            <p class="mb-0"><strong>NRIC </strong> {{$user->retired_personnel->nric }}</p>
-                        </div>
+                    
+                    <div class="mt-5">
+                        <h3><i class="fa fa-medkit" aria-hidden="true"></i> Health
+                            @if(Auth::id() == $user->id)
+                            <a class="text-black" href="{{route('retiredPersonnel.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                            @endif
+                        </h3>
+                        <p class="mb-0"><strong>Fit to work ? </strong> {{$user->retired_personnel->fit_to_work }}</p>
+                        <p class="mb-0"><strong>Have blood pressure ? </strong> {{$user->retired_personnel->have_blood_pressure }}</p>
+                        <p class="mb-0"><strong>Have diabetes ? </strong> {{$user->retired_personnel->have_diabetes }}</p>
+                        <p class="mb-0"><strong>Additional Health Information: </strong> {{$user->retired_personnel->additional_health_statement }}</p>
+                    </div>
+
+                    <div class="mt-5">
+                        <h3><i class="fa fa-info-circle" aria-hidden="true"></i> Other Info
+                            @if(Auth::id() == $user->id)
+                            <a class="text-black" href="{{route('retiredPersonnel.edit', $user->id)}}"> <i class="ml-3 fa fa-pencil-square-o" aria-hidden="true"></i></a>
+                            @endif
+                        </h3>
+                        <p class="mb-0"><strong>Were you government servent ? </strong> {{$user->retired_personnel->government_employee }}</p>
+                        <p class="mb-0"><strong>Prefer Working hours: </strong> {{$user->retired_personnel->full_time ? 'Full Time' : $user->retired_personnel->describe_working_hours}}</p>
+                        <p class="mb-0"><strong>NRIC </strong> {{$user->retired_personnel->nric }}</p>
                     </div>
                 </div>
             </div>
