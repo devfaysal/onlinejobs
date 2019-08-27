adddcbda9855048cd680ea16f80dd5269004b0d2
Faysal Ahamed <miufaysal@live.com>
2019-08-27 13:16:07 +0600

    retired_personnel_educations table incorrect plural laravel 5.8

--- app/RetiredPersonnelEducation.php
+++ app/RetiredPersonnelEducation.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class RetiredPersonnelEducation extends Model
 {
-    //
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'retired_personnel_educations';
 }
