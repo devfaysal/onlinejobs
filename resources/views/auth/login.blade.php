833fcad6f3aedb2c53546aa50ed0b9569a66b201
Faysal Ahamed <miufaysal@live.com>
2019-11-11 12:50:19 +0600

    Login page url updated

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,41 +11,26 @@
 					<div class="card-body signu">
 						<div class="form-group" style="max-width: 180px; margin: auto;">
 							<label class="container">
-							  <input type="radio" checked="checked" name="radio">
+							  <input type="radio" name="radio" onclick="setSignUpLink(this)" value="{{route('employer.register')}}">
 							  <span class="checkmark"></span>Employer
 							</label>
 							<label class="container">
-							  <input type="radio" name="radio">
-							  <span class="checkmark"></span>Agent
+							  <input type="radio" name="radio" onclick="setSignUpLink(this)" value="{{route('agent.create')}}">
+							  <span class="checkmark"></span>Business Partner
 							</label>
 							<label class="container">
-							  <input type="radio" name="radio">
-							  <span class="checkmark"></span>Professional
+							  <input type="radio" name="radio" onclick="setSignUpLink(this)" value="{{route('professional.index')}}">
+							  <span class="checkmark"></span>Job Seeker
 							</label>
 							<label class="container">
-							  <input type="radio" name="radio">
+							  <input type="radio" name="radio" onclick="setSignUpLink(this)" value="{{route('retiredPersonnel.create')}}">
 							  <span class="checkmark"></span>Retired personnel
 							</label>
 						</div>
-						<button type="submit" class="btn reg_btn btn-success btn-block">
+						<a id="signupLink" href="#" class="btn reg_btn btn-success btn-block">
 							Sign up
-						</button>
+						</a>
 						
-						<!---
-						<div class="form-group">
-							<a class="btn btn-primary btn-lg btn-block" href="{{route('employer.register')}}">Employer</a>
-						</div>
-
-						<div class="form-group">
-							<a class="btn btn-warning btn-lg btn-block" href="{{route('agent.create')}}">Business Partner</a>
-						</div>
-						<div class="form-group">
-							<a class="btn btn-info btn-lg btn-block" href="{{route('professional.index')}}">Job Seeker</a>
-						</div>
-						<div class="form-group">
-							<a class="btn btn-secondary btn-lg btn-block" href="{{route('retiredPersonnel.create')}}">Retired Personnel</a>
-						</div>
-						---->
 					</div>
 				</div>
 			</div>
@@ -98,50 +83,6 @@
 								</a>
 							</div>
 							
-							<!--
-							<div class="form-group">
-								<label for="email">{{ __('E-Mail Address') }}</label>
-								<input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="E-mail Address" required>
-
-								@if ($errors->has('email'))
-									<span class="invalid-feedback" role="alert">
-										<strong>{{ $errors->first('email') }}</strong>
-									</span>
-								@endif
-							</div>
-
-							<div class="form-group">
-								<label for="password">{{ __('Password') }}</label>
-								<input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
-
-								@if ($errors->has('password'))
-									<span class="invalid-feedback" role="alert">
-										<strong>{{ $errors->first('password') }}</strong>
-									</span>
-								@endif
-							</div>
-
-							<div class="form-group">
-								<div class="form-check">
-									<input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-									<label class="form-check-label" for="remember">
-										{{ __('Remember Me') }}
-									</label>
-								</div>
-							</div>
-
-							<div class="form-group mb-0 text-center">
-								<button type="submit" class="btn btn-success btn-block">
-									{{ __('Login') }}
-								</button>
-
-								<a class="btn btn-link" href="{{ route('password.request') }}">
-									{{ __('Forgot Your Password?') }}
-								</a>
-							</div>
-							--->
-							{{-- <div class="newuser text-center"><i class="fa fa-user" aria-hidden="true"></i> New User? <a href="{{route('register')}}">Register Here</a></div> --}}
 						</form>
 					</div>
 				</div>
@@ -149,4 +90,16 @@
 		</div>
 	</div>
 </div>
+@endsection
+
+@section('script')
+	<script>
+		function setSignUpLink(val){
+			console.log(val.value);
+			
+			button = document.querySelector('#signupLink');
+			
+			button.href = val.value;
+		}
+	</script>
 @endsection
